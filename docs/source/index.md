961bf1ad00f1f801dde5b124e0446999a3327a3a
vebjornskre <vebjornskre@gmail.com>
2026-02-20 14:05:48 +0100

    Init BEV-MulitMAE project

---
+++ docs/source/index.md
@@ -0,0 +1,3 @@
+## Documentation
+
+Documentation for bev_multimae
---
+++ src/bev_multimae/train.py
@@ -0,0 +1,10 @@
+from bev_multimae.model import Model
+from bev_multimae.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
